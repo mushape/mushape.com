5b62c264ad2743c51c0753076a246081a1b41373
Anderson Ismael <23392311+aicoutodasilva@users.noreply.github.com>
2018-12-13 16:51:27 -0200

    M  public/css/main.css M  view/userGet.php

--- view/userGet.php
+++ view/userGet.php
@@ -9,16 +9,31 @@
     <?php view('inc/nav'); ?>
     <div class="container-fluid">
         <div class="row-fluid">
-            <div class="offset2 span8 userCoverContainer">
-
+            <div class="offset2 span8" id="userCover">
+                <a href="#">
+                    <img src="/img/eu.jpg" alt="" width="175" height="175" class="img-polaroid">
+                </a>
+                <h2>Anderson Ismael</h2>
+            </div>
+        </div>
+        <div class="row-fluid visible-desktop">
+            <div class="offset4 span6" id="userMenu">
+                <ul class="nav nav-pills">
+                    <li><a href="/link1">Link</a></li>
+                    <li><a href="/link2">Link</a></li>
+                    <li><a href="/link3">Link</a></li>
+                </ul>
             </div>
         </div>
         <div class="row-fluid">
             <div class="offset2 span2">
-                <p><?php e($user['description']); ?></p>
-                <p>
-                    <?php e($user['location']); ?>
-                </p>
+                <hr class="visible-phone">
+                <div id="userMeta">
+                    <p><?php e($user['description']); ?></p>
+                    <p>
+                        <?php e($user['location']); ?>
+                    </p>
+                </div>
             </div>
             <div class="span6" id="userFeed">
                 feed do usuario
